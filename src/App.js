9e41426bb19c4a6599f9b74450220aae0c29ce07
rupesh-brs <alajangirupesh@gmail.com>
2024-10-21 11:04:29 +0530

    RockPaperScissorsUsingReactJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './styles.css'
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
 
 function App() {
+  const [boxes, setBoxes] = useState(Array(9).fill(''));
+  const [turnO, setTurnO] = useState(true);
+  const [winner, setWinner] = useState(null);
+  const [count, setCount] = useState(0);
+
+  const handleClick = (index) => {
+    if (boxes[index] !== '' || winner) return;
+
+    const newBoxes = boxes.slice();
+    newBoxes[index] = turnO ? 'O' : 'X';
+    setBoxes(newBoxes);
+    setTurnO(!turnO);
+    setCount(count + 1);
+
+    const isWinner = checkWinner(newBoxes);
+    if (!isWinner && count + 1 === 9) setWinner('Draw');
+  };
+
+  const checkWinner = (boxes) => {
+    for (let pattern of winPatterns) {
+      const [a, b, c] = pattern;
+      if (boxes[a] && boxes[a] === boxes[b] && boxes[a] === boxes[c]) {
+        setWinner(boxes[a]);
+        return true;
+      }
+    }
+    return false;
+  };
+
+  const resetGame = () => {
+    setBoxes(Array(9).fill(''));
+    setTurnO(true);
+    setWinner(null);
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
+      {winner && (
+        <div className="msg-container">
+          <p>{winner === 'Draw' ? 'Game was a Draw.' : `Winner is ${winner}`}</p>
+          <button onClick={resetGame}>New Game</button>
+        </div>
+      )}
+      <div className="game">
+        {boxes.map((box, index) => (
+          <button key={index} className="box" onClick={() => handleClick(index)}>
+            {box}
+          </button>
+        ))}
+      </div>
+      {!winner && <button id="reset-btn" onClick={resetGame}>Reset Game</button>}
     </div>
   );
 }
 
 export default App;
+
